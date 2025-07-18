5127877138fd2c7cfb8e5d6a9463db3155d961ac
gersonbrenof <gersonfagundese2016@gmail.com>
2025-07-18 12:48:53 -0300

    upadte in seeting

--- Anime_API/Anime_API/settings.py
+++ Anime_API/Anime_API/settings.py
@@ -120,13 +120,16 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
-
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
